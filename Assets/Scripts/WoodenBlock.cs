6d64a88ef35dd23a2833495666ed77e23a9b161b
SZTTE <769084783@qq.com>
2019-11-15 11:52:30 +0800

    完善的伤害检测，建筑物初步

---
+++ Assets/Scripts/WoodenBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenBlock : Block
+{
+    public WoodenBlock()
+    {
+        BirdSensitivity.Yellow = 1.5f;
+    }
+
+    protected override void Disappear()
+    {
+        //待完成
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(BirdSensitivity.Red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/classes.cs
+++ Assets/Scripts/classes.cs
@@ -8,6 +8,7 @@ public abstract  class Entity : MonoBehaviour//实体类
     protected Rigidbody2D _rigidbody2D;
     protected Collider2D _collider2D;
     protected Transform _transform;
+    protected Animator _animator;
     public double Mass
     {
         get => _rigidbody2D.mass;
@@ -18,7 +19,7 @@ public abstract  class Entity : MonoBehaviour//实体类
     }
     protected Tube life;
     public Tube Life { get=>life; }
-    
+
     protected Tube state;
     public Tube State => state;
     public abstract void Hurt(int damage);
@@ -29,23 +30,13 @@ public abstract  class Entity : MonoBehaviour//实体类
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _collider2D = GetComponent<Collider2D>();
         _transform = GetComponent<Transform>();
+        _animator = GetComponent<Animator>();
     }
 
     protected void OnCollisionEnter2D(Collision2D other)
     {
-        /*float x1 = _transform.position.x;
-        float y1 = _transform.position.y;
-        float x2 = other.transform.position.x;
-        float y2 = other.transform.position.y;
-        double theta = Math.Atan( (y1 - y2) / (x1 - x2) );
-        double v1 = y1 * Math.Sin(theta) + x1 * Math.Cos(theta);
-        double v2 = y1 * Math.Sin(theta) + x2 * Math.Cos(theta);
-        double m1 = Mass;
-        double m2 = other.rigidbody.mass;
-        double v1E = ((m1 - m2) * v1 + 2 * m2 * v2) / (m1 + m2);
-        double i = m1 * (v1 - v1E);*/
-        
-        Hurt((int) (other.relativeVelocity.magnitude*other.rigidbody.mass*7.4) );
+        ContactPoint2D point  = other.GetContact(0);
+        Hurt((int) point.normalImpulse*50);
     }
 }
 
@@ -57,8 +48,32 @@ public abstract class Bird : Entity
         life.Now -= damage;
         state.Now = life.Now / (life.Full / state.Full) + 1;
         Debug.Log("life now is "+life.Now+"/"+life.Full+",   state now is " + state.Now+"/"+state.Full);
+        _animator.SetInteger("state",state.Now);
         if(life.Now<=0) Disappear();
     }
+}
+public abstract class Block : Entity
+{
+    protected AllBirdsFloat BirdSensitivity;
+    public override void Hurt(int damage)
+    {
+        life.Now -= damage;
+        state.Now = life.Now / (life.Full / state.Full) + 1;
+        Debug.Log("life now is "+life.Now+"/"+life.Full+",   state now is " + state.Now+"/"+state.Full);
+        _animator.SetInteger("state",state.Now);
+        if(life.Now<=0) Disappear();
+    }
+
+    public Block()
+    {
+        BirdSensitivity.Black = 1;
+        BirdSensitivity.Blue = 1;
+        BirdSensitivity.Green = 1;
+        BirdSensitivity.Red = 1;
+        BirdSensitivity.White = 1;
+        BirdSensitivity.Yellow = 1;
+    }
+
 }
 
 public struct Tube//试管型，储存满状态和当前状态
@@ -67,3 +82,15 @@ public struct Tube//试管型，储存满状态和当前状态
     public int Now;//当前状态
 
 }
+
+public struct AllBirdsFloat
+{
+    public float Yellow;
+    public float Red;
+    public float Black;
+    public float Blue;
+    public float White;
+    public float Green;
+}
+
+
